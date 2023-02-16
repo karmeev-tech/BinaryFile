649e58a9544927c047875c85a66ca0ca7bdae06c
Kirill Karmeev <70718637+karmeev-tech@users.noreply.github.com>
2023-02-16 17:12:18 +0300

    Delete Binary directory

--- Binary/Model/Provider.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Binary.Model
-{
-    public abstract class Provider
-    {
-        public abstract string GetProduct();
-        public abstract Task MakeRequest(string path, long position);
-        public string StringValue { get; set; } = null!;
-    }
-}
