0082d67ad84861dbc5c16f1d8b564717221e7161
Kirill Karmeev <70718637+karmeev-tech@users.noreply.github.com>
2023-02-17 13:42:01 +0300

    string reverse add

--- Binary/Model/BasicEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace Binary.Model
-{
-    public class BasicEntity
-    {
-        public string Position { get; set; } = null!;
-        public string Bytes { get; set; } = null!;
-        public string StringRepresentation { get; set; } = null!;
-    }
-}
